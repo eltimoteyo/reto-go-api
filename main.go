d0fe84a39f671b07beec11a2a2cb31ec8bfd00bb
Timoteo Rodriguez <rodrigues_trc_13@hotmail.com>
2022-06-17 18:34:11 -0500

    change url

--- main.go
+++ main.go
@@ -6,15 +6,19 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"time"
+
+	//"path"
 
 	"github.com/gorilla/mux"
+	//"github.com/labstack/echo/v4"
 )
 
 func indexRoute(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Bienvenido al api reto calcular!")
 }
 
-func createTask(w http.ResponseWriter, r *http.Request) {
+func calcular(w http.ResponseWriter, r *http.Request) {
 	var requestNums [][]int
 	var responseNums [][]int
 	reqBody, err := ioutil.ReadAll(r.Body)
@@ -38,9 +42,27 @@ func createTask(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	router := mux.NewRouter().StrictSlash(true)
+	// router := mux.NewRouter().StrictSlash(true)
 
-	router.HandleFunc("/", indexRoute)
-	router.HandleFunc("/calcular", createTask).Methods("POST")
-	log.Fatal(http.ListenAndServe(":3000", router))
+	// router.HandleFunc("/", indexRoute)
+	// router.HandleFunc("/calcular", createTask).Methods("POST")
+	// log.Fatal(http.ListenAndServe(":3000", router))
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", indexRoute)
+	r.HandleFunc("/calcular", calcular)
+
+	srv := &http.Server{
+		Handler: r,
+		Addr:    "https://reto-go-api.herokuapp.com",
+		// Good practice: enforce timeouts for servers you create!
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	log.Fatal(srv.ListenAndServe())
+	// fmt.Println("Go program")
+
+	// server := echo.New()
+	// server.GET(path.Join("/"), indexRoute)
 }
